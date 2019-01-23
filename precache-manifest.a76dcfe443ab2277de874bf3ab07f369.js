7443032a94c219576e4e6aa0deaf0bbb78bd3a28
Shahadat Hossain <hridomheart@gmail.com>
2019-01-23 11:43:59 +0600

    Updates

---
+++ precache-manifest.a76dcfe443ab2277de874bf3ab07f369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0e9fe0af064a8965a2a",
+    "url": "/monico-test/static/css/main.e0346d6d.chunk.css"
+  },
+  {
+    "revision": "f0e9fe0af064a8965a2a",
+    "url": "/monico-test/static/js/main.f0e9fe0a.chunk.js"
+  },
+  {
+    "revision": "df401427a06491c09e05",
+    "url": "/monico-test/static/css/1.b6f0e778.chunk.css"
+  },
+  {
+    "revision": "df401427a06491c09e05",
+    "url": "/monico-test/static/js/1.df401427.chunk.js"
+  },
+  {
+    "revision": "66a619d18b3f048a8792",
+    "url": "/monico-test/static/js/runtime~main.66a619d1.js"
+  },
+  {
+    "revision": "3a0210082a0270a41121b39f11b7e89f",
+    "url": "/monico-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shahadathb/monico-test/precache-manifest.c3d69ed919ae084c6a3f4255a028de3c.js"
+  "/monico-test/precache-manifest.a76dcfe443ab2277de874bf3ab07f369.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shahadathb/monico-test/index.html", {
+workbox.routing.registerNavigationRoute("/monico-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
