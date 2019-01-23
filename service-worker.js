9527698d621dd99e8f123ae1c7fc3d38f03813bd
Shahadat Hossain <hridomheart@gmail.com>
2019-01-23 11:32:59 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1616abf59c01c60f8b3e5a37b8ca386b.js"
+  "/shahadathb/monico-test/precache-manifest.c3d69ed919ae084c6a3f4255a028de3c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shahadathb/monico-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
